21cb71465f465e913c3f0e52e44d983e8eafbba1
nhunimuni <mongnhutran1106@gmail.com>
2018-12-18 23:08:50 +0100

    media query mobile

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ function init() {
     button[0].addEventListener("click", function () {
         var inner = document.getElementsByClassName("flip-card-inner");
         if (isTurned) {
-            inner[0].style.transform = "rotateY(180deg)";
-            isTurned = false;
-            console.log("back");
+          inner[0].style.transform = "rotateY(180deg)";
+          isTurned = false;
+          console.log("back");
         } else {
-            inner[0].style.transform = "rotateY(0deg)";
-            isTurned = true;
-            console.log("front");
+          inner[0].style.transform = "rotateY(0deg)";
+          isTurned = true;
+          console.log("front");
         }
     });
 }
